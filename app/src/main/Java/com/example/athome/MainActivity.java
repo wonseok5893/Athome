af4deaa48a5314fab28dc33c0807cb691f20e80f
gajeong <rlarkwjd127@gmail.com>
2020-04-30 23:16:10 +0900

    Capstone Capstone 1.0.2 카메라,앨범에서 주차구역 이미지 등록

--- app/src/main/Java/com/example/athome/MainActivity.java
+++ app/src/main/Java/com/example/athome/MainActivity.java
@@ -7,34 +7,53 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.navigation.NavigationView;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private DrawerLayout mDrawerLayout;
     private Context context = this;
+    private static NaverMap nm = null;
+    private Button enrollBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(tb);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setHomeAsUpIndicator(R.drawable.menu);
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        enrollBtn = (Button) findViewById(R.id.enroll_button);
+        enrollBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),enrollActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView navigationView = (NavigationView) findViewById(R.id.navi_view);
@@ -47,20 +66,49 @@ public class MainActivity extends AppCompatActivity {
                 int id = menuItem.getItemId();
                 String title = menuItem.getTitle().toString();
                 if (id == R.id.notice) {
-                    Toast.makeText(getApplicationContext(), "공지사항", Toast.LENGTH_LONG).show(); //Intent 처리부분
+                    Toast.makeText(getApplicationContext(), "공지사항", Toast.LENGTH_LONG).show();
                 } else if (id == R.id.reserinfo) {
                     Toast.makeText(getApplicationContext(), "예약내역", Toast.LENGTH_LONG).show();
                 } else if (id == R.id.payment) {
-                    Toast.makeText(getApplicationContext(), "결제,충전,적립", Toast.LENGTH_LONG).show();
-                } else if (id == R.id.account) {
+                    Toast.makeText(getApplicationContext(), "결제•충전•적립", Toast.LENGTH_LONG).show();
+                }else if (id == R.id.account) {
                     Toast.makeText(getApplicationContext(), "계정관리", Toast.LENGTH_LONG).show();
-                } else if (id == R.id.setting) {
-                    Toast.makeText(getApplicationContext(), "환경설정", Toast.LENGTH_LONG).show();
+
+                }else if (id == R.id.setting) {
+                    Toast.makeText(getApplicationContext(), "설정", Toast.LENGTH_LONG).show();
                 }
                 return true;
             }
 
         });
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map_view);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map_view, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        nm = naverMap;
+        naverMap.setMapType(NaverMap.MapType.Navi);
+        Marker marker[] = new Marker[2];
+        for(int i=0;i<2;i++) {
+            marker[i] = new Marker();
+        }
+        marker[0].setPosition(new LatLng(37.5670135, 126.9783740));
+        marker[1].setPosition(new LatLng(40.5670135, 126.9783740));
+        marker[0].setMap(naverMap);
+        marker[1].setMap(nm);
+
+
     }
 
     @Override
@@ -89,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginButtonClicked(View v){
-        Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
+        Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
         //인텐트 실행
         startActivity(intent);
     }
